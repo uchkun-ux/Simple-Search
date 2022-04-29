75d9a61f568c007efe8c4a571f709080c32db508
Uchkun <85468493+uchkun-ux@users.noreply.github.com>
2022-04-29 10:03:02 +0500

    update project

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Employees
+from django.db.models import Q
+
 # Create your views here.
 def home(request):
     if 'q' in request.GET:
         search = request.GET['q']
-        employees = Employees.objects.filter(name__icontains=search)
+        # employees = Employees.objects.filter(surname__icontains=search)
+        total = Q(Q(name__icontains=search)) | Q(Q(surname__icontains=search)) | Q(Q(age__icontains=search)) | Q(Q(job__icontains=search))
+        employees = Employees.objects.filter(total)
     else:
         employees = Employees.objects.all()
         
